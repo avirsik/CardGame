d8e64052363091853cdf77f94b28293a5a466f98
avirsik <annie.virsik@menloschool.org>
2023-11-28 13:38:20 -0800

    Created a Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,35 @@
 public class Card {
+    // instance variables
+    private String rank;
+    private String suit;
+    private int point;
+    // constructor
+    public Card(String rank, String suit, int point){
+        this.rank = rank;
+        this.suit = suit;
+        this.point = point;
+    }
+    // getter and setter methods
+    public String getRank() {
+        return this.rank;
+    }
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    public String getSuit() {
+        return this.suit;
+    }
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    public int getPoint() {
+        return this.point;
+    }
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    // toString method
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
