8c3d43851011ee47656708564c2b40d200255fce
avirsik <annie.virsik@menloschool.org>
2023-12-08 23:58:51 -0800

    Completed ties

--- src/Game.java
+++ src/Game.java
@@ -4,21 +4,22 @@ public class Game {
     // instance variables
     private Player player;
     private Player computer;
-    // creates player's deck
     private Deck deck;
-//    // player's name
     private String name;
-
-    Scanner scan = new Scanner(System.in);
+    private Scanner scan = new Scanner(System.in);
+    private boolean isOver = false;
 
     // constructor
     public Game() {
+        // prints instructions
         printInstructions();
         // creates a deck with cards that have ranks, suits, and points
         String[] ranks = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] suits = {"Hearts", "Clubs", "Spades", "Diamonds"};
         int[] point = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        // initialize deck class
         this.deck = new Deck (ranks, suits, point);
+        // shuffles deck
         deck.shuffle();
         // asks for user's name
         System.out.println("What is your name?");
@@ -35,29 +36,42 @@ public class Game {
     }
 
     public static void printInstructions() {
-        System.out.println("WAR: The deck is divided in half with 26 cards given to each person, face down. The players flip their top cards over and the player with \nthe higher of the two cards will get both cards. If both cards have the same rank, each player will draw 3 cards face down. Each player\nchooses one card to flip over and the player with the higher card gets all the cards.\n\n");
+        System.out.println("INSTRUCTIONS FOR THE GAME OF WAR: \nHalf the deck is given to the player, the other half is given to the computer and just as the cards would be faced down in a normal\n" +
+                                                                "game of war, you will not be able to know the rank or suit of your cards. Flip your top card over and the computer will do the same.\n" +
+                                                                "The player with the the higher rank of the two cards will keep both. Ace counts as 1. The goal is to have all the cards by the end of\n" +
+                                                                "the game. However, if the rank of yours and the computer's cards is the same, you and the computer will randomly choose 1 out of 3\n" +
+                                                                "cards with unknown rank and suit. Whoever has the higher card gets to keep all 8 cards.\n\n");
     }
 
     public void playGame() {
-        Scanner scan = new Scanner(System.in);
-        // while there are things left in the deck
+        // while there are still cards left in the player and computer's hand
         while (!this.player.getHand().isEmpty() && !this.computer.getHand().isEmpty()) {
-            System.out.println(name + ", draw a card. (type 'd')");
-            if (!scan.nextLine().isEmpty()) {
-                // reads player's draw
-                Card pCard = this.player.getHand().remove(0);
-                System.out.println("You drew: " + pCard);
-                // reads computer's draw
-                Card cCard = this.computer.getHand().remove(0);
-                System.out.println("The computer drew: " + cCard+"\n");
-                System.out.println(greatestCard(pCard, cCard));
-                // read player + computer's points
-                System.out.println(name + "'s score: " + player.getPoints());
-                System.out.println("computer's score: " + computer.getPoints() + "\n");
-            }
-            else {
-                checkWon();
-            }
+            compareCards();
+        }
+        // if there is nothing left in the hands
+        checkWon();
+        if (isOver) {
+            return;
+        }
+        return;
+    }
+
+    // compares the user's and computer's cards
+    public void compareCards() {
+        System.out.println(name + ", draw a card. (type 'd')");
+        // when the user draws
+        if (!scan.nextLine().isEmpty()) {
+            // reads player's draw
+            Card pCard = this.player.getHand().remove(0);
+            System.out.println("You drew: " + pCard);
+            // reads computer's draw
+            Card cCard = this.computer.getHand().remove(0);
+            System.out.println("The computer drew: " + cCard);
+            // prints out the greatest card
+            System.out.println(greatestCard(pCard, cCard));
+            // read player + computer's points
+            System.out.println(name + "'s score: " + player.getPoints());
+            System.out.println("computer's score: " + computer.getPoints() + "\n");
         }
     }
 
@@ -65,49 +79,82 @@ public class Game {
     public String greatestCard(Card p, Card c) {
         if (p.getPoint() > c.getPoint()) {
             player.addPoints(2);
-            return "Your Card Wins!\n";
+            return "YOUR CARD WINS!";
         }
         else if (c.getPoint() > p.getPoint()) {
             computer.addPoints(2);
-            return "The Computer's Card Wins!\n";
+            return "THE COMPUTER'S CARD WINS!";
         }
         // if it's a tie
         else {
             cardTie();
         }
-        return "";
-    }
-
-    public void checkWon() {
-        // if player won announce they won
-        if (player.getPoints() > computer.getPoints()) {
-            System.out.println("YOU WON! CONGRATS!!!");
-            return;
-        }
-        // if computer won, announce they won
-        else if (computer.getPoints() > player.getPoints()) {
-            System.out.println("The Computer Won! You Lost!");
-            return;
-        }
-        // if it was a tie, announce the tie and play another round
-        System.out.println("It was a TIE!");
-        playGame();
+        return"";
     }
 
     // deals with ties between cards
     public void cardTie() {
-        Scanner scan = new Scanner(System.in);
+        // tells the user to type 1, 2, or 3 and only accepts those replies
+        System.out.println("IT's A TIE! Pick one card out of the three: (type 1, 2, or 3)\n\nMystery Card 1\nMystery Card 2\nMystery Card 3");
         Card p = this.player.getHand().remove(0);
         Card c = this.computer.getHand().remove(0);
-        System.out.println("IT's A TIE! Pick one card out of the three: (type 1, 2, or 3)\n\nMystery Card 1\nMystery Card 2\nMystery Card 3");
-        String w = scan.nextLine();
-        if (w.equals("1") || w.equals("2") || w.equals("3")) {
+        // if the user picks one of the cards
+        if (!scan.nextLine().isEmpty()) {
+            // check to see if it is a tie in a tie
+            if (p.getPoint() == c.getPoint()) {
+                player.addPoints(4);
+                computer.addPoints(4);
+                System.out.println("IT'S A TIE IN A TIE! YOU AND THE COMPUTER BOTH GET 4 POINTS!");
+                return;
+            }
+            // reads the player's card
             System.out.println("You drew: " + p);
+            // reads the computer's card
+            System.out.println("The computer drew: " + c);
         }
-        System.out.println("The computer drew: " + c);
-        System.out.println(greatestCard(p, c));
+        else {
+            System.out.println("Pick one card out of the three: (type '1', '2', or '3')\n\nMystery Card 1\nMystery Card 2\nMystery Card 3");
+        }
+        // adds correct amount of points
+        if (p.getPoint() > c.getPoint()) {
+            player.addPoints(8);
+            System.out.println("YOUR CARD WINS!");
+        }
+        else if (c.getPoint() > p.getPoint()) {
+            computer.addPoints(8);
+            System.out.println("THE COMPUTER'S CARD WINS!");
+        }
 
-        playGame();
+        for (int i = 0; i < 3; i++) {
+            // while there are cards left in the hand
+            if (!this.player.getHand().isEmpty() && !this.computer.getHand().isEmpty()) {
+                // remove 3 more cards for a total of 8 cards removed
+                this.player.getHand().remove(0);
+                this.computer.getHand().remove(0);
+            }
+            // if there are no cards left in the hand
+            else {
+                checkWon();
+            }
+        }
+    }
+
+    // checks to see if someone has won the entire game
+    public void checkWon() {
+        // if player won announce they won
+        if (player.getPoints() > computer.getPoints()) {
+            System.out.println("YOU WON! CONGRATS!!!");
+            isOver = true;
+            return;
+        }
+        // if computer won, announce they won
+        else if (computer.getPoints() > player.getPoints()) {
+            System.out.println("THE COMPUTER WON! YOU LOST!\nGAME OVER");
+            isOver = true;
+            return;
+        }
+        // if it was a tie, announce the tie and play another round
+        System.out.println("It was a TIE!");
     }
 
     public static void main(String[] args) {
